f3c7f785d06ccc9f9a2a636f90cfee141248e05d
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-21 15:22:12 +0200

    Updated hello.sh

--- azure-deploy-init/scripts/hello.sh
+++ azure-deploy-init/scripts/hello.sh
@@ -1,14 +1,27 @@
 cd /home/juser
-sudo su
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
-mkdir -p /usr/local/bin/
 install minikube /usr/local/bin/
-minikube start --driver=none
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER && newgrp docker
+
+minikube start --driver=docker
 minikube status
-wget https://get.helm.sh/helm-v3.3.0-rc.1-linux-arm64.tar.gz
-tar -zxvf helm-v3.3.0-rc.1-linux-arm64.tar.gz
-mv linux-amd64/helm /usr/local/bin/helm
+wget https://get.helm.sh/helm-v3.3.0-rc.1-linux-amd64.tar.gz
+tar -zxvf helm-v3.3.0-rc.1-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
