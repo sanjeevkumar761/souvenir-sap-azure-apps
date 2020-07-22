5523e1aef54341256a59d16adcfb24e4a351e7c7
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-22 21:35:33 +0200

    Added kubeapps as local source code

--- azure-deploy-init/scripts/hello.sh
+++ azure-deploy-init/scripts/hello.sh
@@ -31,9 +31,25 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
 sudo apt-get update && sudo apt-get -y install socat
-sudo helm repo add bitnami https://charts.bitnami.com/bitnami
+
+git clone https://github.com/sanjeevkumar761/kubeapps.git
+cd kubeapps/chart/kubeapps
+helm chart save . kubeapps
+helm chart save . souveniracr.azurecr.io/helm/kubeapps:v1
+echo $3 | helm registry login souveniracr.azurecr.io \
+  --username $2 \
+  --password-stdin
+helm chart push souveniracr.azurecr.io/helm/kubeapps:v1
+helm chart pull souveniracr.azurecr.io/helm/kubeapps:v1
+helm chart export souveniracr.azurecr.io/helm/kubeapps:v1 \
+  --destination ./install
+
+# sudo helm repo add bitnami https://charts.bitnami.com/bitnami
 sudo kubectl create namespace kubeapps
-sudo helm install kubeapps --namespace kubeapps bitnami/kubeapps --set useHelm3=true
+cd install
+helm dependency update kubeapps
+helm install kubeapps --namespace kubeapps ./kubeapps --set useHelm3=true
+
 sudo kubectl create serviceaccount kubeapps-operator
 sudo kubectl create clusterrolebinding kubeapps-operator --clusterrole=cluster-admin --serviceaccount=default:kubeapps-operator
 kubectl port-forward -n kubeapps svc/kubeapps 8080:80 --address 0.0.0.0
