b300bac0d4c3e1f2f27da87269cecc8ba3bf4f48
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-27 10:24:05 +0200

    index.js for simple table

--- snapshot-manager/src/App.js
+++ snapshot-manager/src/App.js
@@ -4,13 +4,56 @@ import BasicTable from "./BasicTable";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
-const App = () => {
+class App extends Component {
+  state = {
+    products: [],
+    columns: [{
+      dataField: 'id',
+      text: 'Product ID'
+    },
+    {
+      dataField: 'name',
+      text: 'Product Name'
+    }, {
+      dataField: 'price',
+      text: 'Product Price',
+      sort: true
+    }]
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/results')
+      .then(response => {
+        this.setState({
+          products: response.data
+        });
+      });
+  }
+  
+  render() {
+    return (
+      <div className="container" style={{ marginTop: 50 }}>
+        <BootstrapTable 
+        striped
+        hover
+        keyField='id' 
+        data={ this.state.products } 
+        columns={ this.state.columns } />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*const App = () => {
   return (
     <div className="App">
       <h2>Snapshot Manager</h2>
       <BasicTable />
     </div>
   );
-};
+  
+}; 
 
-export default App;
+export default App;*/
