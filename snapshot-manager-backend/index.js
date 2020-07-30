0691476878eba9c5062120920729d889e6145322
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-30 13:49:42 +0200

    Added process env

--- snapshot-manager-backend/index.js
+++ snapshot-manager-backend/index.js
@@ -15,9 +15,9 @@ var ResourceManagementClient = require('azure-arm-resource').ResourceManagementC
 const dotenv = require('dotenv');
 
 _validateEnvironmentVariables();
-var clientId = process.env['CLIENT_ID'];
-var domain = process.env['DOMAIN'];
-var secret = process.env['APPLICATION_SECRET'];
+var clientId = process.env.CLIENT_ID;
+var domain = process.env.DOMAIN;
+var secret = process.env.APPLICATION_SECRET;
 var subscriptionId = process.env['AZURE_SUBSCRIPTION_ID'];
 var resourceClient, computeClient, storageClient, networkClient;
 //Sample Config
@@ -283,10 +283,10 @@ function createVirtualMachine(nicId, vmImageVersionNumber, callback) {
 
 function _validateEnvironmentVariables() {
   var envs = [];
-  if (!process.env['CLIENT_ID']) envs.push('CLIENT_ID');
-  if (!process.env['DOMAIN']) envs.push('DOMAIN');
-  if (!process.env['APPLICATION_SECRET']) envs.push('APPLICATION_SECRET');
-  if (!process.env['AZURE_SUBSCRIPTION_ID']) envs.push('AZURE_SUBSCRIPTION_ID');
+  if (!process.env.CLIENT_ID) envs.push('CLIENT_ID');
+  if (!process.env.DOMAIN) envs.push('DOMAIN');
+  if (!process.env.APPLICATION_SECRET) envs.push('APPLICATION_SECRET');
+  if (!process.env.AZURE_SUBSCRIPTION_ID) envs.push('AZURE_SUBSCRIPTION_ID');
   if (envs.length > 0) {
     throw new Error(util.format('please set/export the following environment variables: %s', envs.toString()));
   }
